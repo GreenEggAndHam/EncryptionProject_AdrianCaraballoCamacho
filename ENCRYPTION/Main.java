220196aa1c98ace9b609d89dab2ea5875e4aaa1f
GreenEggAndHam <adrianc406@nycstudents.net>
2025-12-17 17:29:01 +0000

    I love me some HOT CHOCOLATE

--- ENCRYPTION/Main.java
+++ ENCRYPTION/Main.java
@@ -5,21 +5,28 @@ class Main {
   void print(Object o){ System.out.println(o);}
   void printt(Object o){ System.out.print(o);}
 
-// Adrian and Asael's Encryption Methods
+// Adrian and Asael's Encryption Project
 
 String originale = "Papoy is Cool";
-String reversed = ;
+String reversed = reverse(originale);
+String Ciphered = encodeC(reversed);
+String Letternumber = letternumber(Ciphered);
+String Decryptedletters = decryptLetternumber(Letternumber);
+String Decoded = decodeC(Decryptedletters);
+String Final = reverse(Decoded);
+
   void init(){
 
-    print("Original: " + originale);
-    print("Reversed: " + reverse(originale));
-    print("Encoded: " + encodeC(originale));
-    print("Decoded: " + decode(encodeC(originale)));
-    print("Letter to Number: " + letternumber(originale));
-    print("Decrypted Letters: " + decryptLetternumber(letternumber(originale)));
+    print(originale);
+    print(reversed);
+    print(Ciphered);
+    print(Letternumber);
+    print(Decryptedletters);
+    print(Decoded);
+    print(Final);
 
   }
-  int randomNum = (int)(Math.random() * 20) + 1; 
+ 
   
   // Encoding #1 and Decoding #3: reversing a string
   String reverse(String txt){
@@ -40,21 +47,21 @@ String reversed = ;
     for(int x=0; x<=txt.length()-1; x++){
       ch = txt.charAt(x);
       ascii = (int)ch;
-      ascii += randomNum;
+      ascii += 6;
       build += (char)ascii;
     }     
     return build;
   }
 
   // Decoding #2: Ciphering by random numbers
-  String decode(String txt){
+  String decodeC(String txt){
     String build="";
     int ascii;
     char ch='\0';
     for(int x=0; x<=txt.length()-1; x++){
       ch=txt.charAt(x);
       ascii = (int)ch;
-      ascii -= randomNum;
+      ascii -= 6;
         build += (char)ascii;
     }
     return build;
@@ -63,40 +70,36 @@ String reversed = ;
   //  Encoding #3: Letter to number coding
   // Read the "Letter to number encryption explanation" google doc for the explanation of this function
   String letternumber(String s){
+    String build = "";
     StringBuilder sb = new StringBuilder();
     for(int x = 0; x < s.length(); x++){
       char bats = s.charAt(x);
       if(Character.isLetter(bats)){
-        int num = Character.toUpperCase(bats) - 'A' + 1;
-        sb.append(num);
+        int john = Character.toUpperCase(bats) - 'A' + 1;
+        char hermano = Character.isUpperCase(bats) ? 'U' : 'L';
+        sb.append(john).append(hermano);
       } else {
         sb.append(bats);
       }
       if(x != s.length() - 1) sb.append('-');
     }
-    return sb.toString();
+    return build += sb;
   }
  
  
  // DECODING #1: Number to letter coding
-  // Stringbuilder is able to be changed more than just String.
-  // Appending will add data to the end of it, which is a more efficient way of modifying strings.
-  String decryptLetternumber(String s){
+String decryptLetternumber(String s){
+   String build = "";
     StringBuilder sb = new StringBuilder();
-    String[] parts = s.split("-");
-    for(int i=0;i<parts.length;i++){
-      String p = parts[i];
-      boolean Booster = p.length() > 0;
-      for(int j=0;j<p.length() && Booster;j++) if(!Character.isDigit(p.charAt(j))) Booster = false;
-      if(Booster){
-        int n = Integer.parseInt(p);
-        sb.append((char)('A' + n - 1));
-      } else sb.append(p);
+    for(String p : s.split("-")){
+        if(Character.isDigit(p.charAt(0))){
+            int n = Integer.parseInt(p.replaceAll("[^0-9]", ""));
+            char c = (char)('A' + n - 1);
+            sb.append(p.contains("L") ? Character.toLowerCase(c) : c);
+        } else sb.append(p);
     }
-    return sb.toString();
-  }
-
-
+    return build += sb;
+}
 
 
 }
